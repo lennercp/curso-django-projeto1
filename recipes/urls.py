cab0e09289ae6d0b674a57fd26fa0cf21a1d3b0b
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2022-07-18 21:01:28 -0300

    models

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'recipes'
+app_name = 'recipes' #para fazer com q a url fique recipes/
 
 urlpatterns = [
     path('', views.home, name='home'),
