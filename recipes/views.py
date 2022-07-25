1bcb1ca8dab2603bf1479b0742422555b8e82ceb
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2022-07-25 20:32:36 -0300

    aula 65,66 - ajustar css e get_object

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.factory import make_recipe
 
 from .models import Recipe
@@ -27,9 +27,7 @@ def category(request, category_id):
     })
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        id=id
-    ).order_by('id').first()
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True
