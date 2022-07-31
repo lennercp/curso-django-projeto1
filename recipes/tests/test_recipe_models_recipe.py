f3c0bb8c605458182d5690b08a13fcf2d2149ee0
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2022-07-31 20:13:44 -0300

    aula 93 - testando o default nos fields

--- recipes/tests/test_recipe_models_recipe.py
+++ recipes/tests/test_recipe_models_recipe.py
@@ -1,7 +1,7 @@
 from django.forms import ValidationError
 from parameterized import parameterized
 
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import Recipe, RecipeTestBase
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -9,20 +9,40 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_reciple_titles_raises_error_if_title_has_more_than_65_chars(self):
-        self.recipe.title = 'b' * 66
-
-        with self.assertRaises(ValidationError):
-            self.recipe.full_clean() # AQ UI A VALIDAÇÃO OCORRE
-
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test default category'),
+            author=self.make_author(username='newuser'),
+            title = 'Recipe Title',
+            description = 'Recipe description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutos',
+            servings = 5,
+            servings_unit = 'Porções',
+            preparation_step = 'Recipe Preparation Steps',
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
+ 
     @parameterized.expand([
             ('title', 65),
-            ('description', 165),
+            ('description', 166),
             ('preparation_time_unit', 65),
             ('servings_unit', 65),
         ])
     def test_recipe_field_max_length(self, field, max_length):
 
-        setattr(self.recipe, field, (max_length + 0))
+        setattr(self.recipe, field, 'a' * (max_length + 2))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean() 
+
+    def test_recipe_preparation_step_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(recipe.preparation_step_is_html)
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(recipe.is_published)
+
