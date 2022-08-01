8a8b1370915740f1ba87e149e911374698da9cfc
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2022-08-01 19:49:29 -0300

    aula 99/100 - criando a pagina search com tdd

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,7 @@ app_name = 'recipes' #para identificar de onde a rota será chamada
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('recipes/search/', views.search, name='search'),
     path('recipes/category/<int:category_id>/', views.category, name='category'),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
