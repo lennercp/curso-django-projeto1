4ad7fc1b7becfb90c0b55788a6f9a18dd3e545ad
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2022-08-07 19:39:44 -0300

    104/105/106: arrumando testes e iniciando search

--- recipes/tests/test_recipe_models_recipe.py
+++ recipes/tests/test_recipe_models_recipe.py
@@ -15,7 +15,7 @@ class RecipeModelTest(RecipeTestBase):
             author=self.make_author(username='newuser'),
             title = 'Recipe Title',
             description = 'Recipe description',
-            slug = 'recipe-slug',
+            slug = 'recipe-slug-for-no-defaults',
             preparation_time = 10,
             preparation_time_unit = 'Minutos',
             servings = 5,
