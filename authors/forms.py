bbfdeac896acff2fb388a0ed166f3d068886939d
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2022-12-04 23:20:45 -0300

    organizando arquivos

--- authors/forms.py
+++ authors/forms.py
@@ -25,20 +25,63 @@ def strong_password(password):
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        add_placeholder(self.fields['username'], 'Seu nome de usuario')
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
+
+    username = forms.CharField(
+        label='Username',
+        help_text= (
+            'Username must have letter, numbers or one of those @.+-_. '
+            'The length should be between 4 and 150 characters'
+            ),
+        error_messages={
+            'required': 'This field must not be empty',
+            'min_length': 'Username must have at least 4 characters',
+            'max_length': 'Username must have less than 150 characters'
+        },
+        min_length=4, max_length=150,
+    )
+
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        label='First name'
+    )
+
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last name'
+    )
 
     password = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-                'placeholder': 'Coloque sua senha'
-            }),
-        validators=[strong_password]
+        widget=forms.PasswordInput(),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+        validators=[strong_password],
+        label = 'Password'
     )
     password2 = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Repeat your password'
-        })
+        widget=forms.PasswordInput(),
+        label= 'Password2',
+        error_messages={
+            'required': 'Please, repeat your password'
+        }
+    )
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required'},
+        label='E-mail',
+        help_text='The e-mail must be valid.',
     )
     class Meta:
         model = User
@@ -51,37 +94,25 @@ class RegisterForm(forms.ModelForm):
         ]
         #exclude = ['first_name']
         labels = {
-            'username': 'digite seu usuário'
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password',
         }
-        help_texts = {
-            'email': 'Email deve ser valido'
-        }
-        error_messages = {
-            'username': {
-                'invalid': 'Esse username é invalido'
-            }
-        }
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder': 'Coloque seu primeiro nome',
-                'class': 'form-control'
-            })
-        }
-        # validators = {
-        #     'password': [strong_password]
-        # }
     
     #especifico do campo
-    def clean_password(self):
-        data = self.cleaned_data.get('password')
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
 
-        # if 'atenção' in data: SÓ UM EXEMPLO
-        #     raise ValidationError(
-        #         'Não digite %(value)s no campo password',
-        #         code='invalid',
-        #         params={'value': '"atenção"'}
-        #     )
-        return data
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalid',
+            )
+
+        return email
+
 
     #valida todos os campos, não é um campo especifico
     def clean(self):
